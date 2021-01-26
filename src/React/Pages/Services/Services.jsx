2fffacdee8084f3439d3238247bf377459cda2b8
shivani <shivani.bisaria11@gmail.com>
2021-01-25 20:22:18 -0800

    Scaffolding our Website

---
+++ src/React/Pages/Services/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* Comment H3 ---------------------------*/
+import FilterNav from './FilterNav.jsx';
+import Gallery from './Gallery.jsx';
+
+const Services = () => {
+    return (
+        <div>
+            <h1>Services</h1>
+            <FilterNav />
+            <Gallery />
+        </div>
+    );
+}
+export default Services;
